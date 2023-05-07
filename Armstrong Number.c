70f9f0dc8260d61d09504de35d292482578dfd20
Bandi Murali Chowdary <97547174+bandimuralichowdary@users.noreply.github.com>
2023-05-07 15:13:32 +0530

    Update Armstrong Number.c

--- Armstrong Number.c
+++ Armstrong Number.c
@@ -1,37 +1,34 @@
-#include <stdio.h>
-#include <math.h>
-
-int main() {
-   int number, originalNumber, remainder, result = 0, n = 0;
-   int count;
-
-   printf("Enter an integer: ");
-   scanf("%d", &number);
-
-   originalNumber = number;
-
-   // count the number of digits in the input number
-   while (originalNumber != 0) {
-       originalNumber /= 10;
-       ++n;
-   }
-
-   originalNumber = number;
-
-   // calculate the result
-   while (originalNumber != 0) {
-       remainder = originalNumber % 10;
-       result += pow(remainder, n);
-       originalNumber /= 10;
-   }
-
-   // check if the input number is an Armstrong number
-   if (result == number)
-       printf("%d is an Armstrong number.", number);
-   else
-       printf("%d is not an Armstrong number.", number);
-       
-    count=log(number);
-    printf("\nTime complexity is: %d",count);
-   return 0;
+#include<stdio.h>
+int main()
+{
+	int num,r,sum=0,temp;
+	int count=0;
+	printf("Enter the number:");
+	count++;
+	scanf("%d",&num);
+	count++;
+	for(temp=num;num!=0l;num=num/10)
+	{
+		count++;
+		count++;
+		count++;
+		r=num%10;
+		sum=sum+(r*r*r);
+		count++;
+	}
+	count++;
+	if(sum==temp)
+	{
+		printf("The entered number is ARMSTRONG!\n");
+		count++;
+	}
+	else
+	{
+		printf("The entered number is NOT ARMSTRONG!\n");
+		count++;
+	}
+	count++;
+	printf("Time Complexity is:%d\n",count);
+	count++;
+	return 0;
 }
